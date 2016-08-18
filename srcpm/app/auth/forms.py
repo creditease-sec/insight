577ab683d16b75f2f38ad5945b3a590dff38c2fd
stanford <liusheng318@foxmail.com>
2016-08-18 18:30:58 +0800

    修改密码、重置密码增加密码复杂度验证

--- srcpm/app/auth/forms.py
+++ srcpm/app/auth/forms.py
@@ -56,6 +56,20 @@ class ChangePasswordForm(Form):
     password2 = PasswordField(u'确认密码', validators=[Required()])
     submit = SubmitField(u'修改密码')
 
+    def validate_password(self, field):
+        if len(field.data) < 8 or len(field.data) > 16:
+            raise ValidationError(u'密码位数不满足8到16位之间')
+
+        pattern = re.compile('[A-Za-z]+')
+        match = pattern.findall(field.data)
+        if not match:
+            raise ValidationError(u'密码不包含字母')
+
+        pattern = re.compile('[0-9]+')
+        match = pattern.findall(field.data)
+        if not match:
+            raise ValidationError(u'密码不包含数字')
+
 
 class PasswordResetRequestForm(Form):
     email = StringField(u'邮箱', validators=[Required(), Length(1, 64),
@@ -74,3 +88,17 @@ class PasswordResetForm(Form):
     def validate_email(self, field):
         if LoginUser.query.filter_by(email=field.data).first() is None:
             raise ValidationError(u'此邮箱未注册')
+
+    def validate_password(self, field):
+        if len(field.data) < 8 or len(field.data) > 16:
+            raise ValidationError(u'密码位数不满足8到16位之间')
+
+        pattern = re.compile('[A-Za-z]+')
+        match = pattern.findall(field.data)
+        if not match:
+            raise ValidationError(u'密码不包含字母')
+
+        pattern = re.compile('[0-9]+')
+        match = pattern.findall(field.data)
+        if not match:
+            raise ValidationError(u'密码不包含数字')
